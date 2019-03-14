060a0b0e80275a6e36fdc38d153b420dedad6ed2
Bharathan Mudaliar <vbharathan.2013@gmail.com>
2019-03-14 17:18:20 +0530

    Just added the secret key to the environment variable

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n7a4rp&019#%6$(x8)j()j)-c-vaj1l2#p5i(g!8l1m#ef#7cy'
+#SECRET_KEY = 'n7a4rp&019#%6$(x8)j()j)-c-vaj1l2#p5i(g!8l1m#ef#7cy'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
