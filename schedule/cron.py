659588c8da64ba19591696c238d940e4db3a8020
Bharathan Mudaliar <vbharathan.2013@gmail.com>
2019-03-15 22:11:49 +0530

    added form validation for deadline'

--- schedule/cron.py
+++ schedule/cron.py
@@ -1,6 +1,5 @@
 from django.utils import timezone
 from .models import Schedule
-from django.core.mail import send_mail
 import django_cron 
 
 class SendEmail(django_cron.CronJobBase):
--- schedule/views.py
+++ schedule/views.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Schedule
 from bootstrap_datepicker_plus import DateTimePickerInput
+import datetime
 
 def index(request):
     return render(request, 'schedule/index.html', {'all_schedules':sorted(request.user.schedule_set.all(), key=lambda x: x.deadline) })
@@ -22,6 +23,9 @@ class ScheduleCreate(CreateView):
     
     def form_valid(self, form):
         form.instance.user = self.request.user
+        if form.instance.deadline.date() < datetime.date.today():
+            form.add_error('deadline', 'The Schedule cannot be in the past!')
+            return self.form_invalid(form)
         return CreateView.form_valid(self, form)
     
 class ScheduleUpdate(UpdateView):
@@ -29,10 +33,12 @@ class ScheduleUpdate(UpdateView):
     fields = ['title', 'description', 'deadline']
     template_name = 'schedule/schedule_form.html'
     
-    def get_form(self):
-        form = super().get_form()
-        form.fields['deadline'].widget = DateTimePickerInput()
-        return form
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        if form.instance.deadline.date < datetime.date.today():
+            form.add_error('deadline', 'The Schedule cannot be in the past!')
+            return self.form_invalid(form)
+        return CreateView.form_valid(self, form)
     
     def get_success_url(self):
         return reverse_lazy('schedule:index')    
